300730cc35932be47a0d0dc72e3e1be54f4edb43
nya2020 <52667310+nya2020@users.noreply.github.com>
2019-07-18 11:41:35 -0400

    Add files via upload

---
+++ pairPogram/src/day8/fizzBuzz.java
@@ -0,0 +1,19 @@
+package day8;
+
+public class fizzBuzz  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int x=1; x<100;x++)  {
+		
+		
+				
+
+		
+	    }
+	}  
+	   }
+	}
+}
+}  
---
+++ pairPogram/src/day9/moringEercise.java
@@ -0,0 +1,36 @@
+package day9;
+
+import java.util.Scanner;
+
+public class moringEercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner Scrn = new Scanner (System.in); 
+		
+//	int room = 45;	
+//	int moe = 100;
+	
+		System.out.println("welcome to my calculator");
+		System.out.println("enter a nmuber");
+		int room = Scrn.nextInt();
+			
+			
+for(int x=room; x<=100; x++) {
+			
+		if (room>50) {
+			System.out.println(x);
+			 
+		} 
+//		else if(room>=+50) {
+			
+//	}
+
+}
+}
+
+}
+
+
+		
